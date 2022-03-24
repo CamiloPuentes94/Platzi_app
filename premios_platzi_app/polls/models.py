6e3ea0c121b99ab9361e4453b7160fd46fda86d3
Juanacmilo puentes portatil <jcpsandoval94@gmail.com>
2022-03-24 11:10:40 -0500

    Se realizan cambios para modelos y Choice

--- premios_platzi_app/polls/models.py
+++ premios_platzi_app/polls/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     
+    def __str__(self):
+        return self.question_text
 
-class Choices(models.Model):
+    def was_published_recently(seld):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
-    choices_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
