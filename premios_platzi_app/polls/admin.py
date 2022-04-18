b271fd0697cac9f4f517cad06207ff279d7a6ea5
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-04-18 13:51:52 -0500

    Se agregan modelos para mejorar la interfas del administrador a la hora de crear una pregunta

--- premios_platzi_app/polls/admin.py
+++ premios_platzi_app/polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
